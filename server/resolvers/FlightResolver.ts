635383d76cb70de967399578921aa44a14ec1427
powergym <marco.vander.gracht@student.howest.be>
2021-11-16 09:15:20 +0100

    added addBookedSeat to FlightResolver

--- server/resolvers/FlightResolver.ts
+++ server/resolvers/FlightResolver.ts
@@ -1,6 +1,7 @@
 import { Arg, Authorized, Mutation, Query, Resolver } from 'type-graphql'
 import { getMongoManager, MongoEntityManager } from 'typeorm'
 import { Flight } from '../entities/FlightEntity'
+import { Seat } from '../entities/SeatEntity'
 
 @Resolver()
 export class FlightResolver {
@@ -23,6 +24,19 @@ export class FlightResolver {
   async createFlight(@Arg('data') newFlightData: Flight): Promise<Flight> {
     const flight: Flight = await this.manager.create(Flight, newFlightData)
     this.manager.save(flight)
+    return flight
+  }
+
+  @Mutation(() => Flight, { nullable: true })
+  async addBookedSeat(@Arg('data') newSeatData: Seat, @Arg('flightId') flightId: string): Promise<Flight> {
+    const seat: Seat = await this.manager.create(Seat, newSeatData)
+    const flight: Flight = await this.manager.findOne<Flight>(Flight, { id: flightId })
+
+    flight.bookedSeats.push(seat)
+
+    this.manager.save(seat)
+    this.manager.save(flight)
+
     return flight
   }
 }
